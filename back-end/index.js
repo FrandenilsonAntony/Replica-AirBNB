ebe92e3916977cd3165d1b9bdbe687b230064a9d
Frandenilson Antony <iabyantony88@gmail.com>
2026-02-23 21:14:33 -0400

    Criação arq. Rotas, Model e encryptação do password

--- back-end/index.js
+++ back-end/index.js
@@ -1,18 +1,12 @@
 import "dotenv/config";
 import express from "express";
-import { connectDb } from "./config/db.js";
-import { User } from "./models/User.js";
+import UserRoutes from "./domains/users/routes.js";
 
 const app = express();
-const { PORT, MONGO_URL } = process.env;
+const { PORT } = process.env;
 
-app.get("/users", async (req, res) => {
-  connectDb();
-
-  const usersDoc = await User.find();
-
-  res.json(usersDoc);
-});
+app.use(express.json());
+app.use("/users", UserRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
