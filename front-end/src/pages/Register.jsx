70e908a37fbe6b0c4e5367f9bc5cb99317e3d252
Frandenilson Antony <iabyantony88@gmail.com>
2026-05-03 18:25:29 -0400

    Criação do Files register, conf. e inst. do Axios e Cors

--- front-end/src/pages/Register.jsx
+++ front-end/src/pages/Register.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { Link, Navigate } from "react-router-dom";
 import axios from "axios";
 
-const Login = ({ user, setUser }) => {
+const Register = ({ setUser }) => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
@@ -10,7 +11,7 @@ const Login = ({ user, setUser }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (email && password) {
+    /* if (email && password) {
       try {
         const { data: userDoc } = await axios.post("/users/login", {
           email,
@@ -26,17 +27,24 @@ const Login = ({ user, setUser }) => {
       }
     } else {
       alert("Preencha os campos de email e senha");
-    }
+    } */
   };
 
-  if (redirect || user) return <Navigate to="/" />;
+  if (redirect) return <Navigate to="/" />;
 
   return (
     <section className="flex items-center">
       <div className="mx-auto flex w-full max-w-96 flex-col items-center gap-4">
-        <h1 className="text-3xl font-bold">Faça seu login</h1>
+        <h1 className="text-3xl font-bold">Faça seu cadastro</h1>
 
         <form className="flex w-full flex-col gap-2" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="w-full rounded-full border border-gray-300 px-4 py-2"
+            placeholder="Digite seu nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
           <input
             type="email"
             className="w-full rounded-full border border-gray-300 px-4 py-2"
@@ -53,14 +61,14 @@ const Login = ({ user, setUser }) => {
           />
 
           <button className="bg-primary-400 w-full cursor-pointer rounded-full border border-gray-300 px-4 py-2 font-bold text-white">
-            Login
+            Registrar
           </button>
         </form>
 
         <p>
-          Ainda não possui uma conta?{" "}
-          <Link to="/register" className="text-primary-400">
-            Registre-se!
+          Já tem uma conta?{" "}
+          <Link to="/login" className="text-primary-400">
+            Logue aqui!
           </Link>
         </p>
       </div>
@@ -68,4 +76,4 @@ const Login = ({ user, setUser }) => {
   );
 };
 
-export default Login;
+export default Register;
